8186f18f3e5a0968b402e0ae65933b2fcd39b722
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2018-08-10 09:27:54 -0600

    initial setup finished, server up and running w/ db

---
+++ src/components/Auth.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+// import axios from 'axios'
+
+class Auth extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+            Auth
+            </div>
+        )
+    }
+}
+
+export default Auth
